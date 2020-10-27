b48779e30696e4dbb76ea5f73921000fcf5fb222
Tiago Amaro <tiagoamaro@MacBook-Pro-de-Tiago.local>
2020-10-27 17:38:19 +0000

    sigusr2 handler added

---
+++ aux.h
@@ -0,0 +1,9 @@
+#ifndef AUX_H
+#define AUX_H
+
+#define ARGS_SIZE 5
+
+char * read_user_input_line(void);
+char ** split_input_into_arguments(char * line);
+
+#endif
---
+++ signal.h
@@ -0,0 +1,7 @@
+#ifndef SIGNAL_H
+#define SIGNAL_H
+
+void handle_sigusr1(void);
+void handle_signal(int signal);
+void handle_sigusr2(void);
+#endif
